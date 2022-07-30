a5d73619f0c956f0eb1654a559c88b867a8e7449
Yogendra Pawar <yogendra131994@gmail.com>
2022-07-30 16:29:51 +0530

    gitignore,procfile, updates

--- app.js
+++ app.js
@@ -133,6 +133,13 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
+let port=process.env.PORT;
+if(port===null||port===""){
+  port=3000;
+}
+app.listen(port);
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
